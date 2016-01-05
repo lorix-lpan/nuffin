276347ab1dd5131891fb9a6a12bbed34cfd03c1d
Lawrence Fei Pan <lawrence_pan@hot-shot.com>
2016-01-05 10:21:18 -0500

    add regex to change filename but overall structure is still not organized

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 
+var filename = function (name, date) {
+  var newName = name.replace(/\..+/g, '');
+  var newDate = Math.floor(date / 1000) % 10000000;
+  return newName + '-' + newDate.toString();
+};
+
 var storage = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, './uploads');
   },
   filename: function (req, file, callback) {
-    callback(null, file.originalname + '-' + Date.now());
+    callback(null, filename(file.originalname, Date.now()));
   }
 });
 
@@ -21,13 +27,13 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('/uploads/:id', function (req, res) {
+app.get('/:id', function (req, res) {
   res.sendFile('/srv/http/html/nuffin/uploads/' + req.params.id);
 });
 
 app.post('/', upload.single('file'), function (req, res) {
   console.log(req.file);
-  res.send('localhost:2020/' + req.file.path + '\n');
+  res.send('localhost:2020/' + req.file.filename + '\n');
 });
 
 var server = app.listen(2020, function () {
